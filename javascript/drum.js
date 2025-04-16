6131e9e43f5de730002521925f6df84dc209803a
onyinyeofili <onyinyeofili209@gmail.com>
2025-04-16 02:47:35 +0100

    javascript - ecade (game website)

--- javascript/drum.js
+++ javascript/drum.js
@@ -71,3 +71,25 @@ function buttonAnimation(currentKey) {
     activeButton.classList.remove("pressed");
   }, 100);
 }
+
+const menuIcons = document.querySelectorAll(".menu-icon");
+const sidebar = document.querySelector(".sidebar");
+const overlay = document.querySelector(".overlay");
+const closeIcon = document.querySelector(".close-icon");
+
+menuIcons.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    sidebar.classList.toggle("active");
+    overlay.classList.toggle("active");
+  });
+});
+
+overlay.addEventListener("click", () => {
+  sidebar.classList.remove("active");
+  overlay.classList.remove("active");
+});
+
+closeIcon.addEventListener("click", () => {
+  sidebar.classList.remove("active");
+  overlay.classList.remove("active");
+});
