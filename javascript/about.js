3dd6511a1149eaa525d6b59ddac2883e968eefe3
onyinyeofili <onyinyeofili209@gmail.com>
2025-05-01 10:44:34 +0100

    javascript - ecade (game website)

--- javascript/about.js
+++ javascript/about.js
@@ -21,28 +21,6 @@ closeIcon.addEventListener("click", () => {
   overlay.classList.remove("active");
 });
 
-// infinite scroller
-const scrollers = document.querySelectorAll(".scroller");
-
-if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
-  addAnimation();
-}
-
-function addAnimation() {
-  scrollers.forEach((scroller) => {
-    scroller.setAttribute("data-animated", true);
-
-    const scrollerInner = scroller.querySelector(".scroller__inner");
-    const scrollerContent = Array.from(scrollerInner.children);
-
-    scrollerContent.forEach((item) => {
-      const duplicatedItem = item.cloneNode(true);
-      duplicatedItem.setAttribute("aria-hidden", true);
-      scrollerInner.appendChild(duplicatedItem);
-    });
-  });
-}
-
 // social links
 const socialLinks = document.querySelectorAll(".social-link");
 const ball = document.querySelector(".slider .ball");
--- javascript/collection.js
+++ javascript/collection.js
@@ -79,28 +79,6 @@ sliders.forEach((slider) => {
   });
 });
 
-// services scroller
-const scrollers = document.querySelectorAll(".scroller");
-
-if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
-  addAnimation();
-}
-
-function addAnimation() {
-  scrollers.forEach((scroller) => {
-    scroller.setAttribute("data-animated", true);
-
-    const scrollerInner = scroller.querySelector(".scroller__inner");
-    const scrollerContent = Array.from(scrollerInner.children);
-
-    scrollerContent.forEach((item) => {
-      const duplicatedItem = item.cloneNode(true);
-      duplicatedItem.setAttribute("aria-hidden", true);
-      scrollerInner.appendChild(duplicatedItem);
-    });
-  });
-}
-
 // social links
 const socialLinks = document.querySelectorAll(".social-link");
 const ball = document.querySelector(".slider .ball");
